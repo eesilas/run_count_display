a53dc5cb767e923e3a6e1000f285ebd2540cddc9
Learn by Doing <32452061+eesilas@users.noreply.github.com>
2022-04-05 15:56:22 +0800

    add set yval

--- main.ts
+++ main.ts
@@ -1,20 +1,20 @@
-let xval = 0
 let pace = 0
 let yval = 4
+let xval = 3
 basic.showIcon(IconNames.Heart)
 basic.pause(1000)
 basic.clearScreen()
 loops.everyInterval(1000, function () {
-    xval = 4
-    basic.clearScreen()
     for (let index = 0; index < 5; index++) {
         for (let index = 0; index < 5; index++) {
-            basic.clearScreen()
             led.plot(4, 4 - pace % 5)
-            led.plot(xval, 4 - pace % 5)
             basic.pause(500)
             pace += 1
+            basic.clearScreen()
         }
-        xval += -1
+        led.plot(xval, yval)
+        yval += -1
     }
+    xval += -1
+    yval = 4
 })
